8b6a829bee597a748753c3d0c50315e13d916e04
Caleb Tseng <calebtseng07@gmail.com>
2023-12-15 05:00:11 -0800

    Adding more files to project

---
+++ public/images/index.js
@@ -0,0 +1,21 @@
+import logo from "./CE-Logo.svg";
+
+import contact from "./contact.png";
+
+import warehouse from "./warehouse.png";
+
+import service1 from "./service-1.png";
+import service2 from "./service-2.png";
+import service3 from "./service-3.png";
+import service4 from "./service-4.png";
+
+
+export {
+    logo,
+    contact,
+    warehouse,
+    service1,
+    service2,
+    service3,
+    service4,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+
+export const navLinks = [
+    { href: "#services", label: "Services" },
+    { href: "#results", label: "Results" },
+    { href: "#about", label: "About" },
+    { href: "#contact", label: "Contact" },
+];
