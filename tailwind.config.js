a11a88829d29223650a44da72ee1a92fd7e51f8f
Caleb Tseng <calebtseng07@gmail.com>
2023-12-21 04:05:17 -0800

    Start designing Hero section in services

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-       'roboto': ['roboto', 'sans-serif'],
-       'robotoSlab': ['Roboto Slab', "serif"],
+        'roboto': ['roboto', 'sans-serif'],
+        'robotoSlab': ['Roboto Slab', "serif"],
       },
       colors: {
         green: "#247B46",
+        orange: "#E06127"
       },
+    },
+    fontWeight: {
+      "extra-light": 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      "extra-bold": 800,
+      black: 900,
     },
   },
   plugins: [],
