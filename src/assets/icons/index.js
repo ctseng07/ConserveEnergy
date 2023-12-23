141f23a302ddf8c207b056404afeed056a80f3a4
Caleb Tseng <calebtseng07@gmail.com>
2023-12-23 03:27:32 -0800

    Add media screens to service page

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,10 +1,12 @@
 import hamburger from "./hamburger.svg";
+import hamburgerBlack from "./hamburger-black.svg";
 import phone from "./phone.svg";
 import mail from "./mail.svg";
 
 
 export {
     hamburger,
+    hamburgerBlack,
     phone,
     mail,
 }
